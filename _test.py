cbfd0c93d4d073e308fa3bfaa35ad55c2ac0dc13
FUENTES SUAREZ Alvaro Javier <alvaro.fuentes-suarez@inria.fr>
2018-08-29 13:59:52 +0200

    fixed details in package

--- _test.py
+++ _test.py
@@ -6,20 +6,19 @@ import mesher as ms
 import skeleton as sk
 import field as fl
 
-
-
 def arc_scaff():
+
     C = np.array([0.4,-3.0,10.0])
     r = 4.0
     phi = np.pi*0.6
     u = normalize(np.array([1.0,1.0,0.0]))
     v = normalize(np.array([1.0,-1.0,0.0]))
-
     arc = C,u,v,r,phi
-    nodes = arc_to_nodes(C,u,v,r,phi)
-    arc = nodes_to_arc(*nodes)
 
     g = gr.Graph()
+
+    nodes = arc_to_nodes(C,u,v,r,phi)
+
     for n in nodes:
         g.add_node(n)
     for i in range(3):
@@ -32,7 +31,7 @@ def arc_scaff():
     scaff.compute_scaffold()
 
     arc_skeleton = sk.Arc(C,u,v,r,phi,[1.0,1.0],[2.0,1.0],[1.0,1.0])
-    field = fl.ArcField(1.0,arc_skeleton,)
+    field = fl.ArcField(1.0,arc_skeleton)
 
     mesher = ms.Mesher(scaff,field)
     # mesher.quads_num=1
@@ -48,26 +47,20 @@ def arc_scaff():
 
 def segment_scaff():
 
-    def add_arc(g):
-        C = np.array([0.4,-3.0,10.0])
-        r = 4.0
-        phi = np.pi*0.6
-        u = normalize(np.array([1.0,1.0,0.0]))
-        v = normalize(np.array([1.0,-1.0,0.0]))
+    C = np.array([0.4,-3.0,10.0])
+    r = 4.0
+    phi = np.pi*0.6
+    u = normalize(np.array([1.0,1.0,0.0]))
+    v = normalize(np.array([1.0,-1.0,0.0]))
 
-        nodes = arc_to_nodes(C,u,v,r,phi)
-
-        for n in nodes:
-            g.add_node(n)
-        for i in range(3):
-            g.add_edge(i,i+1)
+    nodes = arc_to_nodes(C,u,v,r,phi)
 
     g = gr.Graph()
-    add_arc(g)
+    for n in nodes:
+        g.add_node(n)
+    for i in range(3):
+        g.add_edge(i,i+1)
 
-    # g.add_node(np.array([0.0,0.0,0.0]))
-    # g.add_node(np.array([5.0,0.0,0.0]))
-    # g.add_edge(0,1)
 
     def get_field(i,j):
         A = g.nodes[i]
@@ -93,6 +86,57 @@ def segment_scaff():
 
     vis.show()
 
+def combined_scaff():
+
+    C = np.array([0.4,-3.0,10.0])
+    r = 4.0
+    phi = np.pi*0.6
+    u = normalize(np.array([1.0,1.0,0.0]))
+    v = normalize(np.array([1.0,-1.0,0.0]))
+
+    nodes = arc_to_nodes(C,u,v,r,phi)
+
+    g = gr.Graph()
+    for n in nodes:
+        g.add_node(n)
+    for i in range(3):
+        g.add_edge(i,i+1)
+
+    g.add_arc([0,1,2,3])
+
+    i = g.add_node(np.array([1.0,2.0,8.0]))
+    g.add_edge(i,0)
+
+    def get_field(i,j):
+        A = g.nodes[i]
+        B = g.nodes[j]
+        n = normalize(np.cross(np.array([1.0,1.0,0.0]),B-A))
+
+        segment_skeleton = sk.Segment.make_segment(A,B,n)
+        return fl.SegmentField(1.0,segment_skeleton)
+
+
+    scaff = sc.Scaffolder(g)
+    scaff.min_subdivs = 16
+    scaff.compute_scaffold()
+
+    fs = [get_field(*e) for e in g.edges]
+
+    arc_skeleton = sk.Arc(C,u,v,r,phi,[1.0,1.0],[2.0,1.0],[1.0,1.0])
+    fs.append(fl.ArcField(1.0,arc_skeleton))
+
+
+    field = fl.MultiField(fs)
+
+    vis = scaff.get_axel_visualization()
+
+    mesher = ms.Mesher(scaff,field)
+    mesher.draw(vis)
+
+    vis.show()
+
+
 if __name__=="__main__":
     # arc_scaff()
-    segment_scaff()
+    # segment_scaff()
+    combined_scaff()
--- skeleton.py
+++ skeleton.py
@@ -1,165 +1,98 @@
 import numpy as np
-import numpy.linalg as nal
+from _math import *
+
+_default_radii = np.ones(2,dtype=float)
 
 class Skeleton(object):
 
-    def __init__(self, lower_bound, upper_bound, initial_weight=1.0, final_weight=1.0):
-        self.lower_bound, self.upper_bound = lower_bound, upper_bound
-        self.initial_weight, self.final_weight = initial_weight, final_weight
+    def __init__(self, l, a, b, c):
 
-    def get_bounds(self):
-        return (self.lower_bound, self.upper_bound)
+        self.l = float(l)
+        self.a = np.array(a)
+        self.b = np.array(b)
+        self.c = np.array(c)
+
+        self._extremities = None
 
     def get_point_at(self, t):
-        if self.lower_bound <= t <= self.upper_bound:
-            return self.compute_point_at(t)
-        else:
-            raise ValueError("Skeleton parameter out of bounds: value %f should be in [%f,%f]" % (t,self.lower_bound,self.upper_bound))
+        raise NotImplementedError()
 
     def get_tangent_at(self, t):
-        if self.lower_bound <= t <= self.upper_bound:
-            return self.compute_tangent_at(t)
-        else:
-            raise ValueError("Skeleton parameter out of bounds: value %f should be in [%f,%f]" % (t,self.lower_bound,self.upper_bound))
-
-    def compute_point_at(self, t):
         raise NotImplementedError()
 
-    def compute_tangent_at(self, t):
+    def get_normal_at(self, t):
         raise NotImplementedError()
 
-    def get_extremal_points(self):
-        return self.get_point_at(self.lower_bound),self.get_point_at(self.upper_bound)
-
-    def get_arc_lenght(self):
+    def get_frame_at(self,t):
         raise NotImplementedError()
+
+    @property
+    def extremities(self):
+        if self._extremities is None:
+            self._extremities = (self.get_point_at(0),self.get_point_at(self.l))
+        return self._extremities
+
 
 class Segment(Skeleton):
     """docstring for Segment"""
-    def __init__(self, P, n, l, w0=1.0, w1=1.0):
-        super(Segment, self).__init__(0,l,w0,w1)
-        self.P = P
-        self.n = n
-        self.l = l
+    def __init__(self, P, v, l, n, a=_default_radii, b=_default_radii, c=_default_radii):
+        super(Segment, self).__init__(l,a,b,c)
 
-        if abs(nal.norm(n)-1.0)>1e-10:
-            raise ValueError('The norm of the vector n=%s is not 1' % n)
+        check_unit(v)
 
-    def compute_point_at(self, t):
-        return self.P+t*self.n
+        self.P = np.array(P)
+        self.v = np.array(v)
+        self.l = float(l)
+        self.n = np.array(n)
 
-    def compute_tangent_at(self,t):
+        self.F = np.matrix([v,n,np.cross(v,n)]).T
+
+    def get_point_at(self, t):
+        return self.P+t*self.v
+
+    def get_tangent_at(self,t):
+        return self.v
+
+    def get_normal_at(self,t):
         return self.n
 
-    def get_arc_lenght(self):
-        return self.l
+    def get_frame_at(self,t):
+        return self.F
 
-    @classmethod
-    def make_segment(klass, A, B, w0=1.0, w1=1.0):
-        n = B-A
-        l = nal.norm(B-A)
-        n /= l
-        return Segment(A,n,l,w0,w1)
+    @staticmethod
+    def make_segment(A,B,n,a=_default_radii,b=_default_radii,c=_default_radii):
+        v = B-A
+        l = norm(v)
+        v /= l
+        return Segment(A,v,l,n,a,b,c)
 
 class Arc(Skeleton):
 
-    def __init__(self,P,r,n1,n2,phi,w0=1.0,w1=1.0):
-        super(Arc, self).__init__(0,phi,w0,w1)
+    def __init__(self,C,u,v,r,phi,a=_default_radii,b=_default_radii,c=_default_radii):
+        super(Arc, self).__init__(phi*r,a,b,c)
 
-        cos_phi = np.cos(phi)
-        self.u_max = np.sqrt((1-cos_phi)/(1+cos_phi))
+        check_unit(u)
+        check_unit(v)
+        check_perp(u,v)
 
-        self.P = P
-        self.r = r
-        self.n1 = n1
-        self.n2 = n2
-        self.phi = phi
+        self.C = np.array(C)
+        self.u = np.array(u)
+        self.v = np.array(v)
+        self.r = float(r)
+        self.phi = float(phi)
 
-        if abs(nal.norm(n1)-1.0)>1e-10:
-            raise ValueError('The norm of the vector n=%s is not 1' % n1)
-        if abs(nal.norm(n2)-1.0)>1e-10:
-            raise ValueError('The norm of the vector n=%s is not 1' % n2)
+        self.binormal = np.cross(u,v)
 
-    def compute_point_at(self, theta):
-        return self.P + self.r*np.cos(theta)*self.n1 + self.r*np.sin(theta)*self.n2
+    def get_point_at(self,t):
+        return self.C + self.r*np.cos(t)*self.u + self.r*np.sin(t)*self.v
 
-    def compute_tangent_at(self, theta):
-        return -np.sin(theta)*self.n1 + np.cos(theta)*self.n2
+    def get_tangent_at(self, t):
+        return -np.sin(t)*self.u + np.cos(t)*self.v
 
-    def get_arc_lenght(self):
-        return self.phi*self.r/(2*np.pi)
+    def get_normal_at(self, t):
+        return -np.cos(t)*self.u - np.sin(t)*self.v
 
-class SkeletonGraph(object):
-    """docstring for SkeletonGraph"""
-
-    class SkeletonNode(object):
-
-        """docstring for SkeletonNode"""
-        def __init__(self, P, idx):
-            super(SkeletonGraph.SkeletonNode, self).__init__()
-            self.P = P
-            self.neighbors = []
-            self.idx = idx
-
-        def add_connection(self, node, skel, field):
-            P = self.P
-            A,B = skel.get_extremal_points()
-            if not nal.norm(P-A)<1e-10 and not nal.norm(P-B)<1e-10:
-                raise ValueError("The node for %s cannot be connected to the node of %s using the skeleton %s",(P,node.P,skel))
-            self.neighbors.append( (node, skel, field) )
-
-
-    def __init__(self):
-        super(SkeletonGraph, self).__init__()
-
-        self.nodes = []
-
-
-    def add_data(self, skel, field):
-        A,B = skel.get_extremal_points()
-        nA = None
-        nB = None
-        for node in self.nodes:
-            if nal.norm(A-node.P)<1e-10:
-                nA = node
-            if nal.norm(B-node.P)<1e-10:
-                nB = node
-            if nA and nB:
-                break
-        if nA is None:
-            nA = SkeletonGraph.SkeletonNode(A,len(self.nodes))
-            self.nodes.append(nA)
-        if nB is None:
-            nB = SkeletonGraph.SkeletonNode(B,len(self.nodes))
-            self.nodes.append(nB)
-        nA.add_connection(nB,skel,field)
-        nB.add_connection(nA,skel,field)
-
-class SkeletonG1PolyCurve(object):
-
-    def __init__(self):
-        super(SkeletonG1PolyCurve, self).__init__()
-        self.pieces = []
-        self.fields = []
-
-    def add_piece(self, skel, field):
-
-        if self.pieces:
-            last = self.pieces[-1]
-
-            A = skel.compute_point_at(skel.lower_bound)
-            B = last.compute_point_at(last.upper_bound)
-            if nal.norm(A-B)>1e-10:
-                raise ValueError("The new piece will break continuity for the skeleton: %s != %s" % (A,B))
-
-            t1 = last.compute_tangent_at(last.upper_bound)
-            t2 = skel.compute_tangent_at(last.lower_bound)
-            if nal.norm(t1-t2)>1e-10:
-                raise ValueError("The new piece will break G1 continuity for the skeleton")
-
-        self.pieces.append(skel)
-        self.fields.append(field)
-
-
+    def get_frame_at(self, t):
+        return np.matrix([self.get_tangent_at(t),self.get_normal_at(t),self.binormal]).T
 
 
